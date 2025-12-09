e4b86ffdd269b09fed4aeef715df67b8779bbb08
CarolinaRibeiro790 <carolribeirog20@gmail.com>
2025-12-09 18:01:37 -0300

    Criação do layout das páginas

---
+++ src/app/transaction/[id].tsx
@@ -0,0 +1,16 @@
+import { Text, View, Button } from "react-native";
+import { useLocalSearchParams, router } from "expo-router";
+
+export default function Transaction() {
+    const params = useLocalSearchParams()
+    return (
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+            <Text>ID: {params.id}</Text>
+
+            <Button
+                title="Voltar"
+                onPress={() => router.back()}
+            />
+        </View>
+    )
+}
